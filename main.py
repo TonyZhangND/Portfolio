a328c85845712467d15769cbc509786323c2e494
Tony Zhang <nz97@cornell.edu>
2019-08-26 15:27:40 -0400

    write fetched data to csv file

--- main.py
+++ main.py
@@ -5,31 +5,32 @@ import time
 
 my_key = ""  # alphavantage key
 portfolio = "portfolio.csv"
+positions = "positions.csv"
 
 
-def summary():
-    """
-    Generates a summary of my portfolio
-    :return: null
-    """
-    my_summary = {}  # map of symbol to total value
+def to_csv():
+    with open(portfolio, "w") as portfolio_file:
+        portfolio_file.write(f"{','.join(['Symbol', 'Shares', 'Price-per-share', 'Total value', 'Timestamp'])}\n")
     ts = TimeSeries(key=my_key, output_format='pandas')
-    with open(portfolio, newline='') as csvfile:
+    with open(positions, newline='') as csvfile:
         rows = list(csv.DictReader(csvfile))
         i = 0
-        print("Symbol\tShares\tPrice-per-share\tTotal value")  # table header
+        print(f"Fetching data for {len(rows)} positions.")  # table header
+        print("\tSymbol\tShares\tPrice-per-share\tTotal value\tTimestamp")  # table header
         for row in enumerate(rows):
             csv_row = row[1]
-            symbol = csv_row['symbol']
-            num_shares = int(csv_row['shares'])
+            symbol = csv_row['Symbol']
+            num_shares = int(csv_row['Shares'])
             data, meta_data = ts.get_intraday(symbol=symbol, interval='1min', outputsize='compact')
             latest_price = data['4. close'].iloc[0]
+            timestamp = data.index.values[0]
             value = latest_price * num_shares
-            my_summary[symbol] = value
+            with open(portfolio, "a") as portfolio_file:
+                portfolio_file.write(f"{symbol},{num_shares},{latest_price:.2f},{value:.2f},{timestamp}\n")
             if len(symbol) < 4:
                 # For formatting purpose
                 symbol = f"{symbol:<4}"
-            print(f"{symbol}\t{num_shares}\t\t{latest_price:.2f}\t\t\t{value:.2f}")
+            print(f"{i}.\t{symbol}\t{num_shares}\t\t{latest_price:.2f}\t\t\t{value:.2f}\t\t{timestamp}")
             i += 1
             if i % 5 == 0 and i < len(rows):
                 # To keep in line with the 5 calls/min rule
@@ -38,7 +39,7 @@ def summary():
 
 
 def main():
-    summary()
+    to_csv()
 
 
 if __name__ == "__main__":
